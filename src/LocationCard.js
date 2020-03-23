7b0c601c2104358b3081bdf53e8af69dc058c403
WordsPerMinute <github@coreyhodge.net>
2020-03-23 15:09:14 -0600

    the map works, add locations now

---
+++ src/LocationCard.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class LocationCard extends React.Component {
+  //your bot army code here...
+
+  renderLocations = (locations) => {
+    return locations.map(location => {return <h2>{location.name}</h2>})
+  }
+
+  render(){
+    return (
+      <div className="location">
+        {this.renderLocations(this.props.locations)}
+      </div>
+    );
+  }
+
+};
+
+export default LocationCard;
---
+++ src/SimpleMap.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 38.8267,
+      lng: -105.7821
+    },
+    zoom: 7
+  };
+
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '50vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: "AIzaSyDMfXFhE7DW1KkdD44LprXEFk5VnNO16Xo"}}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+export default SimpleMap;
